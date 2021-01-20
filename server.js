c0e3b78a7628d46de90248f9ad1fa8771480c846
Dustin Myers <dustint314@gmail.com>
2021-01-20 10:43:10 -0700

    wip

--- server.js
+++ server.js
@@ -1,42 +1,81 @@
-const express = require('express')
-const cors = require('cors')
+// Browsers have a JS engine - chrome JS engine is called V8 Engine
+// NodeJS - JS engine that runs "outside" of the browser 🤯
+// IoT - Internet of Things run node engines - smart fridge, smart toilet
 
-const app = express()
-const port = process.env.PORT || 4000
+const express = require("express");
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
-  if (!req.query.api_key || req.query.api_key !== 'xyz') {
-    res.status(403).json({ message: 'Please supply a valid api_key' })
+  if (!req.query.api_key || req.query.api_key !== "xyz") {
+    res.status(403).json({ message: "Please supply a valid api_key" });
   } else {
-    next()
+    next();
   }
-})
+});
 
 const friends = [
-  { id: '1', name: 'Christopher', email: 'chris@chris.com', age: 32, hobbies: ['coding', 'science fiction', 'sightseeing'] },
-  { id: '2', name: 'Julian', email: 'julian@julian.com', age: 28, hobbies: ['fishing', 'coding', 'death metal'] },
-  { id: '3', name: 'Sofia', email: 'sofia@sofia.com', age: 25, hobbies: ['hiking', 'netflix', 'coding'] },
-  { id: '4', name: 'Joe', email: 'joe@joe.com', age: 22, hobbies: ['heavy metal', 'coding', 'death metal'] },
-  { id: '5', name: 'Hung', email: 'hung@hung.com', age: 35, hobbies: ['reading', 'coding', 'bird watching'] },
-  { id: '6', name: 'Trevor', email: 'trevor@trevor.com', age: 24, hobbies: ['hiking', 'heavy metal', 'coding'] },
-]
+  {
+    id: "1",
+    name: "Christopher",
+    email: "chris@chris.com",
+    age: 32,
+    hobbies: ["coding", "science fiction", "sightseeing"],
+  },
+  {
+    id: "2",
+    name: "Julian",
+    email: "julian@julian.com",
+    age: 28,
+    hobbies: ["fishing", "coding", "death metal"],
+  },
+  {
+    id: "3",
+    name: "Sofia",
+    email: "sofia@sofia.com",
+    age: 25,
+    hobbies: ["hiking", "netflix", "coding"],
+  },
+  {
+    id: "4",
+    name: "Joe",
+    email: "joe@joe.com",
+    age: 22,
+    hobbies: ["heavy metal", "coding", "death metal"],
+  },
+  {
+    id: "5",
+    name: "Hung",
+    email: "hung@hung.com",
+    age: 35,
+    hobbies: ["reading", "coding", "bird watching"],
+  },
+  {
+    id: "6",
+    name: "Trevor",
+    email: "trevor@trevor.com",
+    age: 24,
+    hobbies: ["hiking", "heavy metal", "coding"],
+  },
+];
 
-app.get('/friends/:id', (req, res) => {
-  const friend = friends.find(fr => fr.id === req.params.id)
+app.get("/friends/:id", (req, res) => {
+  const friend = friends.find((fr) => fr.id === req.params.id);
   if (!friend) {
-    res.status(404).json({ message: 'No such friend!' })
+    res.status(404).json({ message: "No such friend!" });
+  } else {
+    res.json(friend);
   }
-  else {
-    res.json(friend)
-  }
-})
+});
 
-app.get('/friends', (req, res) => {
-  res.json(friends.map(fr => ({ id: fr.id, name: fr.name })))
-})
+app.get("/friends", (req, res) => {
+  res.json(friends.map((fr) => ({ id: fr.id, name: fr.name })));
+});
 
 app.listen(port, () => {
-  console.log(`listening on ${port}`)
-})
+  console.log(`listening on ${port}`);
+});
