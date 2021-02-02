e9245c08a9980850492d4f5d85d2263733b458e0
Kalina Allen <kalina.allen@lambdaschool.com>
2021-02-01 18:55:26 -1000

    Solution from guided project

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,56 @@
 // WOW this file sure is empty
+// ICE
+import React, { useEffect, useState } from 'react';
+import Details from './Details';
+import axios from 'axios';
+import { API_KEY, BASE_URL } from '../constants/index';
+
+// 6. component will rerender and friends slice of state will have real data in it
+function App() {
+  // 1. parse through and execute code from top to bottom
+  const [friends, setFriends] = useState([]);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  const closeDetails = () => {
+    setSelectedFriend(null);
+  }
+
+  const fetchFriends = () => {
+    axios.get(`${BASE_URL}/friends/?api_key=${API_KEY}`)
+      .then(response => {
+        console.log(response);
+        // 5. update our friends slice of state
+        setFriends(response.data);
+      })
+      .catch(err => console.log(err))
+  }
+
+  // 2. queue the callbackFn but DO NOT EXECUTE THE callbackFn yet
+  // 4. pull the callbackFn out of the queue, execute it
+  // 7. we will not queue fetchFriends callback again because it was already queued the first time
+       // because of the empty dependency array
+  useEffect(fetchFriends, [])  // callback function, dependency array
+  // useEffect(() => {}, [])
+
+  const Friend = (props) => {
+    return (
+      <div className="friend">
+        {props.friend.name}
+        <button onClick={() => setSelectedFriend(props.friend.id)}>see details</button>
+      </div>
+    )
+  }
+
+  // 3. render the component
+  return (
+    <div className="container">
+      <h1>My Friends:</h1>
+      {
+        friends.map(friend => <Friend key={friend.id} friend={friend}/>)
+      }
+      { selectedFriend && <Details friendId={selectedFriend} close={closeDetails} /> }
+    </div>
+  )
+}
+
+export default App;
--- src/components/Details.js
+++ src/components/Details.js
@@ -7,17 +7,46 @@ export default function Details(props) {
   const [details, setDetails] = useState(null)
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+  // see App.js
 
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
 
+  useEffect(() => {
+    const consoleEventListener = () => {
+      console.log("you clicked me!")
+    }
+    document.addEventListener("click", consoleEventListener)
+
+    return () => {
+      document.removeEventListener('click', consoleEventListener)
+    }
+  }, [friendId])
+
+
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+  // We pretty much never want to do this, but it would be
+  // note the missing dependency array
+  // useEffect(() => {
+  //   console.log("i run after every render")
+  // })
 
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, put the details of the friend in `details` slice of state
+  
+  const fetchDetails = () => {
+    axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+      .then(response => {
+        console.log(response)
+        setDetails(response.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(fetchDetails, [friendId]);
 
   return (
     <div className='container'>
